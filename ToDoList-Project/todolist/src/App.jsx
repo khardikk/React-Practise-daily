ddf5a0a52809295b64f8f073b1bec3426756d83f
Hardikk <kambojpushkal@gmail.com>
2023-11-14 17:47:32 +0530

    Feature : Added Router via react Router Dom

--- ToDoList-Project/todolist/src/App.jsx
+++ ToDoList-Project/todolist/src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <BrowserRouter>
+      
+        <Header />
+    <Routes>
+
+       <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+
+    </Routes>
+      
+    </BrowserRouter>
+   
   );
 }
 
--- ToDoList-Project/todolist/src/Components/Header.jsx
+++ ToDoList-Project/todolist/src/Components/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () => {
-  return <nav>Complete Your Daily Goals</nav>
+  return <>
+  <nav>Complete Your Daily Goals</nav>
+  <div className="header">
+  <Link to='/'>Home</Link>
+  <Link to='/about'>About</Link>
+  <Link to='/contact'>Contact</Link>
+  </div>
+  
+  </>;
   
 };
 
