63d1bad1a2df12812ea2dd751087f7c577b13d78
Hardikk <kambojpushkal@gmail.com>
2023-11-15 22:06:33 +0530

    Data Structuring Done without Real Data

--- NetflixClone-Project/Netflix-Clone/src/Components/Home.jsx
+++ NetflixClone-Project/Netflix-Clone/src/Components/Home.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import "./Home.scss"
 
+const Card = ({img}) => (
+        <img className='card' src = {img} alt = "cover" /> 
+)
+
+const Row = ({title, arr=[{
+     img: "https://lumiere-a.akamaihd.net/v1/images/image_174b2bb6.jpeg?region=0%2C0%2C1400%2C2100"
+}] }) => (
+
+    <div className = "row">
+
+        <h2>{title}</h2>
+
+        <div>
+            {
+
+            arr.map((item) => (
+                <Card img = {item.img} />
+            ))
+            }
+        </div>
+
+    </div>
+)
+
 const Home = () => {
   return(
-    <div>Home</div>
+    <section className = "home">    
+        <div className = "banner"></div>
+
+        <Row title={"Popular on Netflix"}/>
+        <Row title={"Tv Shows"}/>
+        <Row title={"Movies"}/>
+        <Row title={"Recently Added"}/>
+        <Row title={"My Lists"}/>
+    </section>
   )
 }
 
