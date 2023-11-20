7f0d40ae4676250b79e967ad857dde60bf692a0b
Hardikk <kambojpushkal@gmail.com>
2023-11-20 14:00:45 +0530

    UseEffect exercise

--- react-hooks-practice/src/components/Home.jsx
+++ react-hooks-practice/src/components/Home.jsx
@@ -1,9 +1,21 @@
-import React,{useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
   
     // UseState is a hook that allows us to use state in a functional component
     const [count, setCount] = useState(0);
+
+    //UseEffect is a hook that allows us to use lifecycle methods in a functional components
+    useEffect(() => {
+        console.log("useEffect is running");
+
+        return () => {
+
+            console.log("useEffect cleanup");
+            console.log({count});
+            
+        }
+    } , [count])
 
     return (
     <div>
