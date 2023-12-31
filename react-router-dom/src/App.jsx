d0d7a153d5a17c0024d9260075c58159087f99d4
hardikk kamboj <kambojpushkal@gmail.com>
2023-12-31 15:12:05 +0530

    Update page title and add logo assets

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import Header from './components/Header/Header'
-import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     
+      
     </>
   )
 }
